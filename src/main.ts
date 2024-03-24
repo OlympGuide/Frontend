54e13ea3177eac54292ca420d999b292ec29eab1
stofejas <stofejas@students.zhaw.ch>
2024-03-24 17:08:30 +0100

    Add primevue setup and initial demo object

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import { createRouter, createWebHistory } from 'vue-router'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
+import {createRouter, createWebHistory} from 'vue-router'
 
 import './style.css'
 import App from '@/App.vue'
 import NotFound from '@/pages/NotFound.vue'
 import Map from '@/pages/Map.vue'
 
+import 'primevue/resources/themes/lara-light-purple/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+
+import Button from "primevue/button"
+import Dialog from "primevue/dialog";
+
+
 const routes = [
-    { path: '/', component: Map },
-    { path: '/:pathMatch(.*)*', component: NotFound },
+    {path: '/', component: Map},
+    {path: '/:pathMatch(.*)*', component: NotFound},
 ]
 
 const router = createRouter({
@@ -20,6 +28,9 @@ const router = createRouter({
 const pinia = createPinia()
 const app = createApp(App)
 
+// Primevue components, add each component as needed
+app.component('Button', Button);
+app.component('Dialog', Dialog);
 app.use(router)
 app.use(pinia)
 
--- src/pages/Map.vue
+++ src/pages/Map.vue
@@ -1,5 +1,6 @@
 <template>
-    <OlympMap/>
+  <OlympMap/>
+  <Button icon="pi pi-plus" rounded class="absolute bottom-0 z-[1000] m-8 right-0"/>
 </template>
 
 <script setup lang="ts">
