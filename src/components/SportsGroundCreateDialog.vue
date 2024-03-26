3b3a990bb94a1ac06561bcfbcbecfd02852623ab
Jasmin Stofer <61911963+jasminstofer@users.noreply.github.com>
2024-03-26 23:17:05 +0100

    Add save button and error messages

--- src/components/SportsGroundCreateDialog.vue
+++ src/components/SportsGroundCreateDialog.vue
@@ -1,49 +1,128 @@
 <template>
-  <Dialog v-model:visible="visible" @hide="closeDialog" modal header="Edit Profile" class="z-[1000] border-2"
-          :style="{ width: '25rem' }">
-    <span class="p-text-secondary block mb-5">Neuer Sportplatz erstellen</span>
-    <div class="flex align-items-center gap-3 mb-3">
-      <label for="name" class="font-semibold w-6rem">Name</label>
-      <InputText id="name" class="flex-auto" autocomplete="off"/>
-    </div>
-    <div class="flex align-items-center gap-3 mb-5">
-      <label for="description" class="font-semibold w-6rem">Beschreibung</label>
-      <InputText id="description" class="flex-auto" autocomplete="off"/>
-    </div>
-    <div class="flex align-items-center gap-3 mb-5">
-      <label for="coordinates" class="font-semibold w-6rem">Koordinaten</label>
-      <InputText id="coordinates" class="flex-auto" autocomplete="off"/>
+  <Dialog
+    v-model:visible="visible"
+    modal
+    header="Erstelle einen neuen Sportplatz"
+    class="z-[1000] border-2 w-4/5 md:w-3/5 lg:w-1/3"
+    @hide="closeDialog"
+    @show="resetDialog"
+  >
+    <div class="flex-col flex gap-y-5 pt-5">
+      <div class="text-red-300" v-if="errorMessage.length > 0">
+        {{ errorMessage }}
+      </div>
+      <div class="flex flex-col lg:flex-row gap-2">
+        <div class="flex align-items-center gap-3 mb-3">
+          <FloatLabel>
+            <label for="name" class="font-semibold w-6rem">Name</label>
+            <InputText
+              id="name"
+              class="flex-auto"
+              v-model="name"
+              autocomplete="off"
+            />
+          </FloatLabel>
+        </div>
+        <div class="flex align-items-center gap-3 mb-5">
+          <FloatLabel>
+            <label for="coordinates" class="font-semibold w-6rem"
+              >Koordinaten</label
+            >
+            <InputText
+              id="coordinates"
+              class="flex-auto"
+              v-model="coordinates"
+              autocomplete="off"
+            />
+          </FloatLabel>
+        </div>
+      </div>
+      <div class="flex align-items-center gap-3 mb-5">
+        <FloatLabel>
+          <label for="description" class="font-semibold w-6rem"
+            >Beschreibung</label
+          >
+          <InputText
+            id="description"
+            class="flex-auto"
+            v-model="description"
+            autocomplete="off"
+          />
+        </FloatLabel>
+      </div>
     </div>
     <div class="flex justify-content-end gap-2">
-      <Button type="button" label="Cancel" severity="secondary" @click="closeDialog"></Button>
-      <Button type="button" label="Save" @click="closeDialog"></Button>
+      <Button
+        type="button"
+        label="Cancel"
+        severity="secondary"
+        @click="closeDialog"
+      ></Button>
+      <Button
+        type="button"
+        label="Save"
+        :loading="isLoading"
+        @click="submitDialog"
+      ></Button>
     </div>
   </Dialog>
 </template>
 
 <script setup lang="ts">
-import {ref, watch} from "vue";
+import { ref, watch } from "vue";
+import { postSportsGroundData } from "@/api/sportsGroundApi.ts";
+
+const name = ref("");
+const coordinates = ref("");
+const description = ref("");
+const errorMessage = ref("");
+const isLoading = ref(false);
 
 const props = defineProps({
-  isVisible: {type: Boolean, required: true}
-})
+  isVisible: { type: Boolean, required: true },
+});
 
-const emit = defineEmits(["close"])
+const emit = defineEmits(["close"]);
 
-const visible = ref(props.isVisible)
-watch(() => props.isVisible, (value) => {
-  visible.value = value
-})
+const visible = ref(props.isVisible);
+watch(
+  () => props.isVisible,
+  (value) => {
+    visible.value = value;
+  },
+);
 
 const closeDialog = () => {
+  console.log("close Dialog");
+  emit("close");
+};
 
-  console.log("close Dialog")
-  emit("close")
-}
+const resetDialog = () => {
+  name.value = "";
+  coordinates.value = "";
+  description.value = "";
+  errorMessage.value = "";
+};
+
+const submitDialog = async () => {
+  const testObject = {
+    name: name.value,
+    description: description.value,
+    coordinates: coordinates.value,
+  };
+
+  try {
+    isLoading.value = true;
+    errorMessage.value = "";
+    await postSportsGroundData(testObject);
+    closeDialog();
+  } catch (e: any) {
+    console.log("Error while creating new sports ground: ", e);
+    errorMessage.value = "Error while creating new sports ground";
+  } finally {
+    isLoading.value = false;
+  }
+};
 </script>
 
-<style scoped>
-
-</style>
-
-
+<style scoped></style>
--- src/pages/Map.vue
+++ src/pages/Map.vue
@@ -1,21 +1,24 @@
 <template>
-  <OlympMap/>
-  <Button @click="openDialog" icon="pi pi-plus" rounded class="absolute bottom-0 z-[1000] m-8 right-0"/>
-  <SportsGroundCreateDialog @close="visible = false" :is-visible="visible"/>
+  <OlympMap />
+  <Button
+    icon="pi pi-plus"
+    rounded
+    class="absolute bottom-0 z-[1000] m-8 right-0"
+    @click="openDialog"
+  />
+  <SportsGroundCreateDialog :is-visible="visible" @close="visible = false" />
 </template>
 
 <script setup lang="ts">
-import OlympMap from '@/components/OlympMap.vue';
-import {ref} from "vue";
+import OlympMap from "@/components/OlympMap.vue";
+import { ref } from "vue";
 import SportsGroundCreateDialog from "@/components/SportsGroundCreateDialog.vue";
 
-const visible = ref(false)
+const visible = ref(false);
 
 const openDialog = () => {
-  visible.value = true
-}
-
+  visible.value = true;
+};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
