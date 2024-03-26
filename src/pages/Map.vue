d94d08145c17ed4f076c4a967ebdb3daa8002515
Jasmin Stofer <61911963+jasminstofer@users.noreply.github.com>
2024-03-26 10:17:40 +0100

    Add initial popup dialog structure

--- src/pages/Map.vue
+++ src/pages/Map.vue
@@ -1,10 +1,20 @@
 <template>
   <OlympMap/>
-  <Button icon="pi pi-plus" rounded class="absolute bottom-0 z-[1000] m-8 right-0"/>
+  <Button @click="openDialog" icon="pi pi-plus" rounded class="absolute bottom-0 z-[1000] m-8 right-0"/>
+  <SportsGroundCreateDialog @close="visible = false" :is-visible="visible"/>
 </template>
 
 <script setup lang="ts">
 import OlympMap from '@/components/OlympMap.vue';
+import {ref} from "vue";
+import SportsGroundCreateDialog from "@/components/SportsGroundCreateDialog.vue";
+
+const visible = ref(false)
+
+const openDialog = () => {
+  visible.value = true
+}
+
 </script>
 
 <style scoped>
