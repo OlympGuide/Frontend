70ee5d540d195f8269b4cce678a974aeca060192
Jasmin Stofer <61911963+jasminstofer@users.noreply.github.com>
2024-04-04 20:22:56 +0200

    Add new pages which can be accessed through the sidebar

---
+++ src/pages/Login.vue
@@ -0,0 +1,44 @@
+<template>
+  <Dialog
+    modal
+    header="Login"
+    class="z-[1000] border-2 w-4/5 md:w-3/5 lg:w-3/6"
+    v-model:visible="visible"
+  >
+    <FloatLabel class="w-full relative">
+      <label for="name" class="font-semibold w-full"
+      >Name <span class="text-red-800">*</span></label
+      >
+      <InputText
+          id="name"
+          class="flex-auto w-full"
+          :class="{ 'p-invalid': emailError }"
+          v-model="email"
+          autocomplete="off"
+      />
+      <small class="p-error input-error">{{ emailError }}</small>
+    </FloatLabel>
+  </Dialog>
+</template>
+
+<script setup lang="ts">
+import OlympMap from "@/components/OlympMap.vue";
+import {useField, useForm} from "vee-validate";
+const { handleSubmit, validate, resetForm } = useForm({
+  initialValues: {
+    email: '',
+    password: '',
+  },
+  validateOnMount: false,
+});
+
+const { value: email, errorMessage: emailError } = useField<string>(
+    'email',
+    'required'
+);
+
+const visible = true;
+
+</script>
+
+<style scoped></style>
---
+++ src/pages/Reservations.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="page-container-placement"><p class="page-title">Meine Reservationen</p></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
