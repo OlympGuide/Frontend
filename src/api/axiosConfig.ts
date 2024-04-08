a159b618b7da8d8ebd886ac2d5e3f9a448bf6768
Cyrill Neuschwander <cneuschwander@swisslex.ch>
2024-04-08 22:07:15 +0200

    added import-meta-env

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Access the base URL from Vite's environment-specific configuration
-const baseURL = import.meta.env.VITE_API_BASE_URL;
+const baseURL = import.meta.env.API_BASE_URL;
 
 const apiClient = axios.create({
   baseURL: baseURL, // Use the dynamically assigned base URL
