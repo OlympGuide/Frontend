560addf6fd1ec35a2efbb076243ff9d008280eaf
Jasmin Stofer <61911963+jasminstofer@users.noreply.github.com>
2024-04-10 17:17:43 +0200

    Add address completion input field using openstreetmap

---
+++ src/utils/Debounce.ts
@@ -0,0 +1,13 @@
+export function debounce(fn: Function, delay: number) {
+    let timeoutId: number | undefined;
+
+    return (...args: any) => {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+
+        timeoutId = window.setTimeout(() => {
+            fn(...args);
+        }, delay);
+    };
+}
