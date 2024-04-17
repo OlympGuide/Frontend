22595556d64594a03fe27cff8cf5daded0ab86d7
Cyrill Neuschwander <cneuschwander@swisslex.ch>
2024-04-17 21:11:56 +0200

    made ApiClient singleton

--- src/api/sportFieldApi.ts
+++ src/api/sportFieldApi.ts
@@ -1,17 +1,17 @@
-import { apiClient } from '@/api/axiosConfig.ts';
+import { getApiClientInstance } from '@/api/axiosConfig.ts';
 import { PostSportField, SportField } from '@/types/Map.ts';
 import { AxiosResponse } from 'axios';
 
 const SPORT_FIELD_PATH = '/sportfields/';
 
 export const postSportField = async (data: PostSportField) => {
-  const api = await apiClient();
+  const api = await getApiClientInstance();
   return api.post(SPORT_FIELD_PATH, data);
 };
 
 export const getSportFields = async (): Promise<
   AxiosResponse<SportField[], any>
 > => {
-  const api = await apiClient();
+  const api = await getApiClientInstance();
   return api.get<SportField[]>(SPORT_FIELD_PATH);
 };
--- src/api/sportFieldProposalApi.ts
+++ src/api/sportFieldProposalApi.ts
@@ -1,20 +1,20 @@
-import { apiClient } from '@/api/axiosConfig.ts';
+import { getApiClientInstance } from '@/api/axiosConfig.ts';
 import { PostSportFieldProposal, SportFieldProposal, SportFieldProposalState } from '@/types/Proposal';
 import { AxiosResponse } from 'axios';
 
 const PROPOSAL_PATH = '/sportfieldproposals/';
 
 export const postSportFieldProposal = async (data: PostSportFieldProposal) => {
-  const api = await apiClient();
+  const api = await getApiClientInstance();
   return api.post(PROPOSAL_PATH, data);
 };
 
 export const getSportFieldProposals = async (): Promise<AxiosResponse<SportFieldProposal[], any>> => {
-  const api = await apiClient();
+  const api = await getApiClientInstance();
   return api.get<SportFieldProposal[]>(PROPOSAL_PATH);
 };
 
 export const changeStateById = async (id: string, state: SportFieldProposalState) => {
-  const api = await apiClient();
+  const api = await getApiClientInstance();
   return api.put(`${PROPOSAL_PATH}${id}`, { state });
 };
