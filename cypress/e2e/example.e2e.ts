1c218a980d659276cd181826b10daaaa98e109b8
Jasmin Stofer <61911963+jasminstofer@users.noreply.github.com>
2024-04-26 10:08:20 +0200

    Create example test

--- cypress/e2e/example.e2e.ts
+++ cypress/e2e/example.e2e.ts
@@ -1,6 +1,14 @@
-describe('Example Component', () => {
-  it('should open create sportfields dialog', () => {
+describe('Example Test', () => {
+  beforeEach('Go to dashboard', () => {
     cy.visit('/');
-    cy.get('#triggerButton').should('be.visible').click();
+  });
+  it('should open create sportfields dialog', () => {
+    cy.get('[data-cy=sportsfield-dialog-button]').should('be.visible').click(); //use data-cy here because there is no id assigned
+
+    cy.get('#name').type('Sportfield test');
+    cy.get('#coordinates').type('47, 8');
+    cy.get('#description').type('This is my first cypress test description');
+
+    cy.get('[data-cy=speichern-button]').click();
   });
 });
