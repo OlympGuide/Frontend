cf34185a99bb9fde391a8f4a8861a2c2a7f42cee
Dario Aubry <dario.aubry@gmail.com>
2024-04-29 19:17:46 +0200

    improvements

--- src/stores/SportFieldStore.ts
+++ src/stores/SportFieldStore.ts
@@ -5,6 +5,7 @@ import { AxiosResponse } from 'axios';
 import { ApiState } from '@/types/ApiState.ts';
 import { getReservationsBySportField } from '@/api/reservationApi.ts';
 import { Reservation } from '@/types/Reservation.ts';
+import { addHours } from 'date-fns';
 
 interface SportFieldState extends ApiState {
   sportFields: SportField[];
@@ -55,10 +56,36 @@ export const useSportFieldStore = defineStore('sportField', {
 
       try {
         this.errorMessage = '';
-        const res: AxiosResponse<Reservation[], any> =
-          await getReservationsBySportField(id);
+        // TODO: Implement API call to get reservations by sport field when ready
+        // const res: AxiosResponse<Reservation[], any> =
+        //   await getReservationsBySportField(id);
 
-        this.selectedSportField!.reservations = res.data;
+        this.selectedSportField!.reservations = [
+          {
+            id: 'blabla1',
+            user: {
+              id: '12e2abab-dfc8-4c95-acd1-f80c7d42541b',
+              name: 'Dario Aubry',
+              displayName: 'Dario Aubry',
+              roles: [0],
+            },
+            start: new Date(),
+            end: addHours(new Date(), 1),
+          },
+          {
+            id: 'blabla2',
+            user: {
+              id: 'aaaaaaa-dfc8-4c95-acd1-f80c7d42541b',
+              name: 'Max Mustermann',
+              displayName: 'Max Mustermann',
+              roles: [0],
+            },
+            start: addHours(new Date(), 3),
+            end: addHours(new Date(), 4),
+          },
+        ];
+
+        // this.selectedSportField!.reservations = res.data;
       } catch (e: any) {
         console.error('Error while loading sport field reservations: ', e);
         this.errorMessage = 'Es gab ein Problem beim Übermitteln der Daten';
