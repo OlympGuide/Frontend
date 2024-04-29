b53c8fcdbdcff15f09de45aacfd1eda42428c6dd
Dario Aubry <dario.aubry@gmail.com>
2024-04-29 19:17:46 +0200

    improvements

--- src/pages/Reserve.vue
+++ src/pages/Reserve.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="page-container-placement">
-    <p class="page-title">Reservieren</p>
-
     <ScheduleXCalendar :calendar-app="calendarApp" class="calendar">
       <template
         #timeGridEvent="{ calendarEvent }: { calendarEvent: CalendarEvent }"
@@ -9,12 +7,15 @@
         <div class="event">
           {{ getEventTime(calendarEvent.start, calendarEvent.end) }}
 
-          <div class="flex gap-1 absolute right-1">
+          <div
+            v-if="calendarEvent.isEditable"
+            class="flex gap-1 absolute right-1"
+          >
             <a @click="reserve(calendarEvent)"
               ><i class="pi pi-check" style="font-size: 0.7rem"></i
             ></a>
             <a @click="cancel(calendarEvent)"
-              ><i class="pi pi-times" style="font-size: 0.7rem"></i
+              ><i class="pi pi-trash" style="font-size: 0.7rem"></i
             ></a>
           </div>
         </div>
@@ -27,12 +28,8 @@
 import { ScheduleXCalendar } from '@schedule-x/vue';
 import { createCalendar, viewWeek, CalendarEvent } from '@schedule-x/calendar';
 import '@schedule-x/theme-default/dist/index.css';
-import {
-  formatEventTime,
-  toLocalDate,
-  toStartOfHour,
-} from '@/services/dateService.ts';
-import { onMounted, ref } from 'vue';
+import { formatEventTime, toStartOfHour } from '@/services/dateService.ts';
+import { onMounted } from 'vue';
 import { PostReservation, Reservation } from '@/types/Reservation.ts';
 import { useSportFieldStore } from '@/stores/SportFieldStore.ts';
 import { useRoute } from 'vue-router';
@@ -40,12 +37,13 @@ import { addHours, format } from 'date-fns';
 import { createDragAndDropPlugin } from '@schedule-x/drag-and-drop';
 import { createResizePlugin } from '@schedule-x/resize';
 import { useReservationStore } from '@/stores/ReservationStore.ts';
+import { useUserStore } from '@/stores/UserStore.ts';
+import { instanceOfUser, User } from '@/types/User.ts';
 
 const sportFieldStore = useSportFieldStore();
 const reservationStore = useReservationStore();
+const userStore = useUserStore();
 const route = useRoute();
-
-const reservations = ref<Reservation[]>([]);
 
 const calendarApp = createCalendar({
   locale: 'de-CH',
@@ -59,6 +57,12 @@ const calendarApp = createCalendar({
   weekOptions: {
     gridHeight: 700,
   },
+  me: {
+    colorName: 'me',
+  },
+  others: {
+    colorName: 'others',
+  },
   callbacks: {
     async onClickDateTime(dateTime: string) {
       const startDateTime = formatEventTime(toStartOfHour(new Date(dateTime)));
@@ -67,6 +71,8 @@ const calendarApp = createCalendar({
         id: Date.now().toString(),
         start: startDateTime,
         end: formatEventTime(addHours(new Date(startDateTime), 1)),
+        isEditable: true,
+        calendarId: 'me',
       };
 
       calendarApp.events.add(event);
@@ -76,36 +82,30 @@ const calendarApp = createCalendar({
   plugins: [createDragAndDropPlugin(), createResizePlugin()],
 });
 
-onMounted(() => {
+onMounted(async () => {
   const sportFieldId: string = route.params.id as string;
-  sportFieldStore.loadSelectedSportField(sportFieldId);
+  await sportFieldStore.loadReservationsBySportField(sportFieldId);
+  const reservations: Reservation[] =
+    sportFieldStore.selectedSportField?.reservations || [];
 
-  reservations.value = [
-    {
-      id: 'blabla',
-      user: {
-        id: 'user',
-        name: 'Dario Aubry',
-        displayName: 'Dario Aubry',
-        roles: [0],
-      },
-      start: new Date(),
-      end: addHours(new Date(), 1),
-    },
-  ];
-
-  const events = reservationsToCalenderEvents();
+  const events = reservationsToCalenderEvents(reservations);
   calendarApp.events.set(events);
 });
 
-const reservationsToCalenderEvents = (): CalendarEvent[] => {
-  return reservations.value.map((reservation: Reservation) => {
-    return {
-      id: reservation.id,
-      start: formatEventTime(reservation.start),
-      end: formatEventTime(reservation.end),
-    };
-  });
+const reservationsToCalenderEvents = (
+  reservations: Reservation[]
+): CalendarEvent[] => {
+  return (
+    reservations?.map((reservation: Reservation) => {
+      return {
+        id: reservation.id,
+        start: formatEventTime(reservation.start),
+        end: formatEventTime(reservation.end),
+        isEditable: isMyReservation(reservation),
+        calendarId: isMyReservation(reservation) ? 'me' : 'others',
+      };
+    }) || []
+  );
 };
 
 const getEventTime = (start: string, end: string): string => {
@@ -115,6 +115,7 @@ const getEventTime = (start: string, end: string): string => {
 const reserve = (event: CalendarEvent) => {
   if (!sportFieldStore.selectedSportField) {
     console.error('selectedSportField is undefined!');
+    // TODO: implement error handling
     return;
   }
 
@@ -129,6 +130,14 @@ const reserve = (event: CalendarEvent) => {
 
 const cancel = (event: CalendarEvent) => {
   calendarApp.events.remove(event.id);
+};
+
+const isMyReservation = (reservation: Reservation): boolean => {
+  if (!instanceOfUser(userStore.user)) {
+    return false;
+  }
+
+  return reservation.user.id === userStore.user.id;
 };
 </script>
 
