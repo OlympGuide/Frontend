983e6ddc8fbf9a5612cdf372f32c2b46ddf7eab1
Cyrill Neuschwander <cneuschwander@swisslex.ch>
2024-05-05 15:56:22 +0200

    improved style in settings

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -2,18 +2,19 @@
   <div class="page-container-placement">
     <p class="page-title">Einstellungen</p>
     <div class="mt-5 flex items-center space-x-4">
-      <p class="text-lg font-medium">Demo Mode</p>
-      <ToggleButton
-        v-model="isDemoActive"
-        class="w-24"
-        onLabel="On"
-        offLabel="Off"
-      />
-      <Button
-        @click="testDataButtonClicked"
-        class="w-24">
-        Testdaten (Neu)erstellen
-      </Button>
+      
+      <div class="button-layout flex flex-col space-y-2">
+        <ToggleButton
+          v-model="isDemoActive"
+          class="w-48"
+          onLabel="Demo Mode: On"
+          offLabel="Demo Mode: Off" />
+        <Button
+          @click="testDataButtonClicked"
+          class="w-48 !bg-primaryRed">
+          Testdaten (Neu)erstellen
+        </Button>
+      </div>
     </div>
   </div>
 </template>
