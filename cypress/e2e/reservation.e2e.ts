b2b84e84562eb6393a1ce48c313a457d018d9850
Jasmin Stofer <61911963+jasminstofer@users.noreply.github.com>
2024-05-16 14:57:37 +0200

    Add test to cover my reservations page

--- cypress/e2e/reservation.e2e.ts
+++ cypress/e2e/reservation.e2e.ts
@@ -4,20 +4,69 @@ describe('Make a reservation', () => {
       cy.login('cypress@olympguide.ch', 'CypressTest1$');
     });
 
-    it('should make a reservation', () => {
+    it('should create and delete a reservation', () => {
+      cy.get('#map').click(900, 895);
+      cy.get('[data-cy=button-reserve]').click();
+      cy.get('div.sx-vue-calendar-wrapper.calendar')
+        .should('be.visible')
+        .click(300, 400);
+
+      cy.wait(500);
+
+      cy.get('div.event.me')
+        .first()
+        .within(() => {
+          cy.get('.pi-check').click();
+        });
+
+      cy.get('div.event.me')
+        .first()
+        .within(() => {
+          cy.get('.pi-trash').click();
+        });
+
+      cy.get('.p-toast-message-success').should('be.visible');
+      cy.get('.p-toast-detail').should(
+        'contain',
+        'Ihre Reservation wurde gelöscht'
+      );
+    });
+
+    it('should create a reservation', () => {
       cy.get('#map').click(900, 895);
       cy.get('[data-cy=button-reserve]').click();
       cy.get('div.sx-vue-calendar-wrapper.calendar').click();
 
-      cy.get('div.event.me').within(() => {
-        cy.get('.pi-check').click();
-      });
+      cy.get('div.event.me')
+        .first()
+        .within(() => {
+          cy.get('.pi-check').click();
+        });
 
       cy.get('.p-toast-message-success').should('be.visible');
 
       cy.get('div.event.me').should('exist');
 
-      cy.get('div.event.me').within(() => {
+      cy.get('[data-cy="menu-item-Reservationen"]')
+        .should('be.visible')
+        .click();
+
+      cy.get('table').contains('td', '12:00');
+    });
+
+    it('should delete a reservation', () => {
+      cy.get('[data-cy="menu-item-Reservationen"]')
+        .should('be.visible')
+        .click();
+
+      cy.get('table')
+        .contains('td', '12:00')
+        .then((tableCell) => {
+          const tableRow = tableCell.closest('tr');
+          cy.wrap(tableRow).find('.p-button').click();
+        });
+
+      cy.get('div.event.me-selected').within(() => {
         cy.get('.pi-trash').click();
       });
 
