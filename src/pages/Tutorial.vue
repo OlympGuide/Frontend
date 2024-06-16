865dbf9f1e2f487af088ed86db21010474b51ffb
Noah Schlauri <schlanoa@students.zhaw.ch>
2024-06-16 20:46:28 +0200

    verschönert

--- src/pages/Tutorial.vue
+++ src/pages/Tutorial.vue
@@ -2,12 +2,19 @@
   <div class="page-container-placement">
     <p class="page-title">OlympGuide Tutorial</p>
 
-    <h2>1. Startseite</h2>
-    <p>Wenn Sie OlympGuide besuchen, landen Sie zunächst auf der Startseite. Diese ist in zwei Hauptbereiche unterteilt:
-    </p>
+    <h2>1. Home</h2>
+    <p>Die Homepage des OlympGuides besteht aus der grossen Interaktiven Karte und einem Menu, welche alle nötigen Funktionen beinhaltet. </p>
     <br>
-    <ul class="list-disc">
-      <li><strong>Menu:</strong><br> Auflistung aller verfügbaren Funktionen</li>
+    <ul>
+      <li><strong>Menu:</strong><br> Auflistung aller verfügbaren Funktionen
+        <ul class="sublist">
+          <li>Login</li>
+          <li>Interaktive Karte</li>
+          <li>Reservationen</li>
+          <li>Einstellungen</li>
+          <li>Tutorial</li>
+        </ul>
+      </li>
       <li><strong>Karte:</strong><br> Eine interaktive Karte, die die wichtigste Funktionalität von OlympGuide
         darstellt.</li>
     </ul>
@@ -15,11 +22,10 @@
     <br><br>
     <br><br>
     <h2>2. Karte</h2>
-    <p>Auch ohne Anmeldung können Sie die Karte durchsuchen und Sportplätze finden. Um dies zu erleichtern, steht Ihnen
-      ein Filter zur Verfügung:</p>
+    <p>Auf der Karte können sie beliebige Sportplätze finden. Um dies zu erleichtern, steht Ihnen ein Filter zur Verfügung:</p>
     <br>
-    <ul class="list-disc">
-      <li><strong>Filteroptionen:</strong><br> Sie können nach Sportart oder Verfügbarkeit filtern.</li>
+    <ul>
+      <li><strong>Filteroptionen:</strong><br> Sie können nach der Sportart filtern um  ihre Suche zu vereinfachen.</li>
       <li><strong>Interaktive Karte:</strong><br> Die Karte passt sich den gewählten Filtern an. Klicken Sie auf einen
         Sportplatz, um Details zu sehen.</li>
       <li><strong>Hinweis:</strong> Die Option zur Sportplatzreservation ist nur nach dem Login verfügbar.</li>
@@ -37,7 +43,7 @@
     <h2>3. Reservationen</h2>
     <p>Um eine Reservierung vorzunehmen, gehen Sie wie folgt vor:</p>
     <br>
-    <ul class="list-disc">
+    <ul>
       <li><strong>suchen:</strong><br> Suchen Sie Sportplätze in ihrer Umgebung</li>
       <li><strong>filtern:</strong><br> Der interaktive Filter erleichtert ihre Suche</li>
       <li><strong>auswählen:</strong><br> Wählen sie einen Platz aus und lesen sie seine Details</li>
@@ -56,7 +62,7 @@
     <p>Neben der Nutzung öffentlicher Sportplätze haben Sie auch die Möglichkeit, eigene Sportplätze im OlympGuide
       anzumelden:</p>
     <br>
-    <ul class="list-disc">
+    <ul>
       <li><strong>anfragen:</strong><br>  Geben Sie alle notwendigen Daten ein</li>
       <li><strong>prüfen:</strong><br> Ihre Anfrage wird geprüft, um Fehleinträge und Duplikationen zu vermeiden.</li>
       <li><strong>veröffentlichen:</strong><br> Bei erfolgreicher Prüfung wird Ihr Sportplatz im Portal sichtbar.</li>
@@ -83,6 +89,10 @@
 
   .tutorial-videos {
     @apply flex justify-center;
+  }
+
+  .sublist {
+    @apply list-inside list-disc pl-4;
   }
 
   li {
